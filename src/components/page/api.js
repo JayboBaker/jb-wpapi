609f38f833ad0d232547f4e7fd0d47fc1a7a05fa
James Baker <james.baker@crunch.co.uk>
2017-05-22 09:18:13 +0100

    JB - JB Fix JSON request mock

--- src/components/page/api.js
+++ src/components/page/api.js
@@ -1,26 +1,10 @@
 import request from 'superagent'
-import { wpApiUrl } from '../../app/constants'
+import path from 'path'
 
-// const pagesEndPoint = `${wpApiUrl}/wp-json/wp/v2/pages` // Endpoint for getting Wordpress Pages
+const mockEndpoint = path.resolve(__dirname, './mock/allPages.json')
 
-// <<<<<<< HEAD
-// const mockEndpoint = '../../../mock/allPages.json'
-// =======
-const mockEndpoint = './mock/allPages.json'
 export function fetchPage (slug) {
   return request
     .get(`${mockEndpoint}`)
     .then(res => res.body.filter(x => x.slug === slug))
 }
-
-// >>>>>>> develop
-// export function fetchPage (slug) {
-//   return request
-//     .get(`${mockEndpoint}`)
-//     .then(res => res.body.filter(x => x.slug === slug))
-// }
-// // export function fetchPage (slug) {
-// //   return request
-// //     .get(`${pagesEndPoint}/?slug=${slug}`)
-// //     .then(res => res.body)
-// // }
